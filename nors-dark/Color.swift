a5993f5a5d87916ceac47a3c6b638122491e3c76
Neo Mode <neomode@list.ru>
2022-02-20 11:43:07 +0300

    update colors

--- nors-dark/Color.swift
+++ nors-dark/Color.swift
@@ -8,18 +8,15 @@
 import UIKit
 
 enum Color {
-	static let white = UIColor(red: 0.969, green: 0.973, blue: 0.98, alpha: 1)
-	static let black = UIColor(red: 0.159, green: 0.187, blue: 0.213, alpha: 1)
-	static let blue = UIColor(red: 0.345, green: 0.624, blue: 0.973, alpha: 1)
+	static let white = UIColor(red: 0.968627, green: 0.972549, blue: 0.980392, alpha: 1)
+	static let black = UIColor(red: 0.156863, green: 0.188235, blue: 0.211765, alpha: 1)
+	static let blue = UIColor(red: 0.345098, green: 0.623529, blue: 0.972549, alpha: 1)
 
-	static let lightGray = UIColor(red: 0.774, green: 0.785, blue: 0.797, alpha: 1)
-	static let middleGray = UIColor(red: 0.558, green: 0.59, blue: 0.635, alpha: 1)
+	static let lightGray = UIColor(red: 0.772549, green: 0.784314, blue: 0.796078, alpha: 1)
+	static let middleGray = UIColor(red: 0.556863, green: 0.588235, blue: 0.635294, alpha: 1)
 	static let middleGray2 = UIColor(red: 0.196, green: 0.212, blue: 0.263, alpha: 1)
-	static let darkGray = UIColor(red: 0.587, green: 0.609, blue: 0.629, alpha: 1)
-	static let darkBlue = UIColor(red: 0.267, green: 0.328, blue: 0.385, alpha: 1)
-
-	static let facebook = UIColor(red: 0.231, green: 0.349, blue: 0.596, alpha: 1)
-	static let twitter = UIColor(red: 0, green: 0.675, blue: 0.929, alpha: 1)
+	static let darkGray = UIColor(red: 0.588235, green: 0.607843, blue: 0.627451, alpha: 1)
+	static let darkBlue = UIColor(red: 0.266667, green: 0.329412, blue: 0.384314, alpha: 1)
 }
 
 extension CGFloat {
